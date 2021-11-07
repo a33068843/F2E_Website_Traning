73b6af5200bf2a189f266d7d781cf2353522dafc
Benny Jou <a33068843@gmail.com>
2021-11-08 01:45:13 +0800

    feat: 首頁搜尋欄拉皮

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-console.log("test")
+// 首頁搜尋欄
+const typeDropdown = $('#searchTypeDropdown');
+const typeDropdownDefault = typeDropdown.find('.default .text')
+const typeDropdownOptions = typeDropdown.find('.options li')
+typeDropdown.on('click', function() {
+  typeDropdown.toggleClass('active');
+})
+typeDropdownOptions.on('click', function() {
+  const target = $(this).find('.text').text();
+  typeDropdownDefault.text(target);
+})
