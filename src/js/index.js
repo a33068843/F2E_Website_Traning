b3a12b7ce217c65f419813517df3b4924aad43dc
Benny Jou <a33068843@gmail.com>
2021-11-10 22:17:11 +0800

    fix: 調整 Dropdown 的元件更擴充

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,40 @@
-// 首頁搜尋欄
-const typeDropdown = $('#searchTypeDropdown');
-const typeDropdownDefault = typeDropdown.find('.default .text')
-const typeDropdownOptions = typeDropdown.find('.options li')
-typeDropdown.on('click', function() {
-  typeDropdown.toggleClass('active');
-})
-typeDropdownOptions.on('click', function() {
-  const target = $(this).find('.text').text();
-  typeDropdownDefault.text(target);
-})
+// 搜尋元件
+function searchDropdown(dropdown) {
+  const typeDropdownDefault = dropdown.find('.default .text')
+  const typeDropdownOptions = dropdown.find('.options li')
+  dropdown.on('click', function() {
+    dropdown.toggleClass('active');
+  })
+  typeDropdownOptions.on('click', function() {
+    const target = $(this).find('.text').text();
+    typeDropdownDefault.text(target);
+  })
+}
 
-// 滑動圖片
 $(document).ready(function() {
-  $('#a').lightSlider({
-    item: 1,
-    loop:true,
-    auto: false,
-    addClass: 'customSlider',
-    pauseOnHover: true,
-    pause: 3000,
-    slideMargin: 0
-  });
+  // 搜尋元件判斷處
+  const kindDropdown = $('#searchDropdown_kind');
+  const hasKindDropdown = !!kindDropdown.length;
+  if (hasKindDropdown) searchDropdown(kindDropdown);
+
+  const cityDropdown = $('#searchDropdown_city');
+  const hasCityDropdown = !!cityDropdown.length;
+  if (hasCityDropdown) searchDropdown(cityDropdown);
+
+  const topicDropdown = $('#searchDropdown_topic');
+  const hasTopicDropdown = !!topicDropdown.length;
+  if (hasTopicDropdown) searchDropdown(topicDropdown);
+
+  // 滑動圖片
+  if ($('#a').length) {
+    $('#a').lightSlider({
+      item: 1,
+      loop:true,
+      auto: false,
+      addClass: 'customSlider',
+      pauseOnHover: true,
+      pause: 3000,
+      slideMargin: 0
+    });
+  }
 });
