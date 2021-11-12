9de899859678bb692b40e986659a4605854033a6
Benny <benny_tsao@gss.com.tw>
2021-11-12 18:15:52 +0800

    feat: 新增搜尋相關頁面和一點搜尋功能

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,98 @@
+// const datas = [
+//   {
+//     "ID": "C1_A25000000E_100045",
+//     "Name": "聯經書房 上海書店",
+//     "DescriptionDetail": "聯經出版公司位於新生南路上的門市，不過並不侷限於自家出版品。一樓聯經書房的書籍以人文社科類為主，地下室的上海書店則是銷售簡體字書籍。除台北外，聯經在台中等地亦設有門市書店。",
+//     "Phone": "02-23620308#10(聯經書房);#15(上海書店)",
+//     "Address": "臺北市  大安區新生南路三段94號1樓、B1",
+//     "TravelInfo": "離聯經書房最近的捷運站是新店線(綠線)的公館站，從三號出口出來後沿著臺灣大學的圍牆往前走，走到臺大正門後就能看到位於校門斜前方的聯經書房了。",
+//     "OpenTime": "週一~週五：11:00-20:00；週六、週日：12:00-21:00",
+//     "Picture": {
+//       "PictureUrl1": "http://cloud.culture.tw/h_upload_ccacloud/festival/image/A0/B0/C0/D0/E0/F125/d472d173-bc0b-4eec-aa0c-44bf46e17e63.jpg"
+//     },
+//     "Position": {
+//       "PositionLon": 121.53315734863281,
+//       "PositionLat": 25.01752471923828,
+//       "GeoHash": "wsqqmd39x"
+//     },
+//     "Class1": "藝術類",
+//     "WebsiteUrl": "http://www.linkingbooks.com.tw/LNB/index.aspx",
+//     "ParkingPosition": {},
+//     "City": "臺北市",
+//     "SrcUpdateTime": "2021-11-12T01:11:18+08:00",
+//     "UpdateTime": "2021-11-12T02:40:14+08:00"
+//   },
+//   {
+//     "ID": "C1_A25000000E_100044",
+//     "Name": "艾比露比外文書店",
+//     "DescriptionDetail": "艾比露比外文書店是一間社區型書店。書店內書籍以英文兒童與成人繪本為大宗，並且不定期於店內舉辦女性成長、親職教養與英文師訓講座。感恩節至農曆新年這幾個月，也會固定舉辦義賣、物資捐贈與不定期至偏鄉或弱勢機構免費講故事活動。",
+//     "Phone": "02-2747-7808",
+//     "Address": "臺北市  松山區饒河街175號2樓",
+//     "TravelInfo": "捷運松山站2號出口，饒河夜市內",
+//     "OpenTime": "本書店已於2018年完成網路系統化訂購，辦公室與倉庫不開放選書、付費及取書。",
+//     "Picture": {
+//       "PictureUrl1": "http://cloud.culture.tw/h_upload_ccacloud/festival/image/A0/B0/C0/D0/E0/F124/ca36ee4c-19d4-4703-ade8-8c98dbc7de25.jpg"
+//     },
+//     "Position": {
+//       "PositionLon": 121.575927734375,
+//       "PositionLat": 25.050640106201172,
+//       "GeoHash": "wsqqw82fs"
+//     },
+//     "Class1": "藝術類",
+//     "WebsiteUrl": "http://www.abbyrubybooks.com",
+//     "ParkingPosition": {},
+//     "City": "臺北市",
+//     "SrcUpdateTime": "2021-11-12T01:11:18+08:00",
+//     "UpdateTime": "2021-11-12T02:40:14+08:00"
+//   },
+//   {
+//     "ID": "C1_A25000000E_100043",
+//     "Name": "鴻儒堂書局",
+//     "DescriptionDetail": "創立於1936年，專門經營日文書籍及日文雜誌，及各種日語學習教材、台日關係研究書籍等。同時有日本最新的出版資料供讀者查詢，並可代理訂購。",
+//     "Phone": "02-2311-3810；02-2311-3823",
+//     "Address": "臺北市  中正區台北市博愛路九號五樓502室",
+//     "TravelInfo": "捷運台北車站懷寧街（Z8）出口，星巴克旁溫州大餛飩巷內",
+//     "OpenTime": "週一～週六：上午 10 : 00 - 下午 18 : 00 止 / 國定假日：上午 11 : 00 - 下午 18 : 00 止 (中午 12 : 00 - 下午 13 : 00 午休 / 星期日公休)",
+//     "Picture": {
+//       "PictureUrl1": "http://cloud.culture.tw/h_upload_ccacloud/festival/image/A0/B0/C0/D0/E0/F123/a9cd880d-4875-40a0-99da-2a58bf3a41f9.JPG"
+//     },
+//     "Position": {
+//       "PositionLon": 121.51375579833984,
+//       "PositionLat": 25.04623794555664,
+//       "GeoHash": "wsqqmpe8g"
+//     },
+//     "Class1": "藝術類",
+//     "WebsiteUrl": "http://www.hjtbook.com.tw",
+//     "ParkingPosition": {},
+//     "City": "臺北市",
+//     "SrcUpdateTime": "2021-11-12T01:11:18+08:00",
+//     "UpdateTime": "2021-11-12T02:40:14+08:00"
+//   },
+//   {
+//     "ID": "C1_A25000000E_100042",
+//     "Name": "大陸書店",
+//     "DescriptionDetail": "大陸書店展售中外音樂書籍、鋼琴、小提琴、吉他…等各種樂器的學程教材及樂譜；手作藝術類書籍、期刊－－裁縫、毛線編織、壓花、十字繡、不織布、黏土、刺繡、拼布、串珠…等，更收藏了逸品級的『書道全集』等書法字帖、繪本，每每讓人有『挖寶般』流連忘返的新鮮感。",
+//     "Phone": "886-2-23113914",
+//     "Address": "臺北市  中正區衡陽路79號3F",
+//     "TravelInfo": "捷運西門站4號出口",
+//     "OpenTime": "週一～週日 09:00 ～ 20:00  (除夕至初五休)",
+//     "Picture": {
+//       "PictureUrl1": "http://cloud.culture.tw/h_upload_ccacloud/festival/image/A0/B0/C0/D0/E0/F121/56bb41c3-e7e3-4d27-8aff-7a87ddd0e1b7.jpg"
+//     },
+//     "Position": {
+//       "PositionLon": 121.51099395751953,
+//       "PositionLat": 25.04233741760254,
+//       "GeoHash": "wsqqmncd5"
+//     },
+//     "Class1": "藝術類",
+//     "WebsiteUrl": "https://www.talubook.com/",
+//     "ParkingPosition": {},
+//     "City": "臺北市",
+//     "SrcUpdateTime": "2021-11-12T01:11:18+08:00",
+//     "UpdateTime": "2021-11-12T02:40:14+08:00"
+//   }
+// ]
+
 // 搜尋元件
 function searchDropdown(dropdown) {
   const typeDropdownDefault = dropdown.find('.default .text')
@@ -10,8 +105,108 @@ function searchDropdown(dropdown) {
     typeDropdownDefault.text(target);
   })
 }
+// 拿到搜尋參數
+function getSettings() {
+  
+}
+// 搜尋結果個數
+function checkResults() {
+  const checkResults = $('#checkResults');
+  const hasResults = checkResults.find('.card_simple').length
+  const simpleWrapper = checkResults.find('.card_simpleWrapper')
+  const emptyWrapper = checkResults.find('.card_emptyWrapper')
+  if (hasResults) {
+    if (!simpleWrapper.is(":visible")) simpleWrapper.toggle();
+    if (!!emptyWrapper.is(":visible")) emptyWrapper.toggle();
+    return;
+  }
+  if (!!simpleWrapper.is(":visible")) simpleWrapper.toggle();
+  if (!emptyWrapper.is(":visible")) emptyWrapper.toggle();
+}
+// 取得授權
+function getAuthority() {
+  const id = 'e7d52bd12c9d4392b114afddbde9e654'
+  const key = 'x1kLGusSVGFE3vnaLUgiQYwGcfo'
+  const GMTString = new Date().toGMTString();
+  const ShaObj = new jsSHA('SHA-1', 'TEXT');
+  ShaObj.setHMACKey(key, 'TEXT');
+  ShaObj.update('x-date: ' + GMTString);
+  const HMAC = ShaObj.getHMAC('B64');
+  const Authorization = 'hmac username=\"' + id + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+  return { 'Authorization': Authorization, 'X-Date': GMTString };
+}
+// 搜尋功能
+const localSettings = JSON.parse(localStorage.getItem('setting'));
+function search(localSettings, callApi) {
+  const {
+    kindSetting   = null,
+    citySetting   = null,
+    topicSetting  = null,
+    inputSetting  = null,
+    dataCount,
+  } = localSettings;
+  switch(callApi) {
+    case 'recentScene':
+      recentScene();
+      break;
+  }
+  $('.breadcrumbs li').last().text(kindSetting);
+}
+
+// 將不同地方用到的 API 分出來
+function recentScene() {
+  const selectDom = $('#recentScene').find('.card_simpleWrapper');
+  const render = (data) => {
+    return (`
+      <div class="card_simple">
+        <div class="img">
+          <img src="${data.Picture.PictureUrl1 ? data.Picture.PictureUrl1 : "../images/noImage_x2.png"}">
+        </div>
+        <div class="title">${data.Name}</div>
+        <div class="locationWrapper">
+          <div class="icon"></div>
+          <div class="text">${data.Address.substring(0,3)}</div>
+        </div>
+      </div>
+    `)
+  }
+
+  // const result = datas.flatMap((data) => {
+  //   return render(data);
+  // })
+  // selectDom.html(result);
+  // axios.get(
+  //   `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$orderby=ID%20desc&$top=4&$format=JSON`,
+  //   { headers: getAuthority() }
+  // ).then((response) => {
+  //   const datas = response.data;
+  //   const item = datas.flatMap((data) => {
+  //     return render(data);
+  //   })
+  //   selectDom.html(item);
+  // });
+
+}
 
 $(document).ready(function() {
+  const currentPage = location.pathname;
+  if (currentPage === '/result.html') search();
+
+  console.log('get');
+  console.log(localStorage.getItem('setting'));
+  const a = !!localStorage.getItem('search');
+  console.log(a);
+  const localSettings = JSON.parse(localStorage.getItem('setting'));
+  console.log(localSettings);
+  localStorage.clear();
+
+  // 首頁串 API
+  const hasRecentScene = !!$('#recentScene').length;
+  if (hasRecentScene) {
+    const searchCondition = {};
+    search(searchCondition, 'recentScene');
+  }
+
   // 搜尋元件判斷處
   const kindDropdown = $('#searchDropdown_kind');
   const hasKindDropdown = !!kindDropdown.length;
@@ -25,6 +220,33 @@ $(document).ready(function() {
   const hasTopicDropdown = !!topicDropdown.length;
   if (hasTopicDropdown) searchDropdown(topicDropdown);
 
+  // 拿取搜尋設定
+  
+  // 搜尋結果個數
+  checkResults();
+  const searchButton = $('.searchButton')
+
+  searchButton.on('click', function() {
+    const kindSetting = $('.breadcrumbs').length ?
+      $('.breadcrumbs li').last().text() : kindDropdown.find('.default .text').text();
+    const citySetting = cityDropdown.find('.default .text').text()
+    const topicSetting = topicDropdown.find('.default .text').text()
+    const inputSetting = $('.searchInput').val();
+    const settings = {
+      "kindSetting": kindSetting,
+      "citySetting": citySetting,
+      "topicSetting": topicSetting,
+      "inputSetting": inputSetting,
+    }
+    localStorage.setItem('setting', JSON.stringify(settings))
+    checkResults();
+    if (currentPage !== '/result.html') {
+      localStorage.setItem('search', true);
+      location.href = './result.html';
+    }
+  })
+  checkResults()
+  //
   // 滑動圖片
   if ($('#a').length) {
     $('#a').lightSlider({
